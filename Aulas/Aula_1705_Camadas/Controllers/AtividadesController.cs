622249a2ca49a340abaae5eb7ba1d4f1fd5c4f19
Aluno <Aluno@DOSO125250>
2018-05-17 09:01:27 -0300

    BuscaPorID completo

--- Aulas/Aula_1705_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula_1705_Camadas/Controllers/AtividadesController.cs
@@ -9,12 +9,37 @@ namespace Aula_1705_Camadas.Controllers
 {
     class AtividadesController
     {
+        //Lista
         public List<Atividade> ListaAtividades { get; set; }
 
         //Salvar
+        public void Salvar(Atividade atividade  )
+        {
+            ListaAtividades.Add(atividade); //pega a lista e salva
+        }
+
         //Listar
-        //Ler
+        public List<Atividade> Listar()
+        {
+            return ListaAtividades;
+        }
+
+        //BuscarPorID
+        public Atividade BuscarPorID(int id)
+        {
+            foreach (Atividade a in ListaAtividades)
+            {
+                if(a.AtividadeID == id)
+                {
+                    return a;
+                }
+            }
+
+            return null; //se ele nao entrar no if, quer dizer que ele nao achou atividade
+        }
+
         //Editar
+
         //Exclui
 
     }

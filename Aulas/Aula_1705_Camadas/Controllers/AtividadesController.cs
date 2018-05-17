9eaf35f7a7c25db32067276d1b42a476a1e43685
Aluno <Aluno@DOSO125250>
2018-05-17 08:46:43 -0300

    Adicionando using para Lista e Atividade

---
+++ Aulas/Aula_1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,21 @@
+﻿using Aula_1705_Camadas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        public List<Atividade> ListaAtividades { get; set; }
+
+        //Salvar
+        //Listar
+        //Ler
+        //Editar
+        //Exclui
+
+    }
+}
---
+++ Aulas/Aula_1705_Camadas/Models/Atividade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula_1705_Camadas.Models
+{
+    class Atividade
+    {
+        //controller - código
+        //
+        //view - estrutura de formularios    
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}

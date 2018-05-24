c2baf19b4211245646823db11aa6034f806dc42c
Aluno <Aluno@DOSO125250>
2018-05-24 09:33:29 -0300

    Criando view

--- ControleAtividades/Modelos/Categoria.cs
+++ ControleAtividades/Modelos/Categoria.cs
@@ -9,6 +9,8 @@ namespace Modelos
     class Categoria
     {
         public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
 
     }
 }
